5031d8c8a0f775fd3dc524c1c65715bf7be66ce1
DI_bin <44898339+dibin666@users.noreply.github.com>
2023-06-07 00:41:13 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,17 +7,10 @@ if [ "$choice" = "y" ]; then
   curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
 fi
 
-# 删除 KernelSU/kernel/ksu.c 关于KPROBES依赖的警告 （部分内核不删除会error）
-read -p "是否删除KernelSU/kernel/ksu.c 关于KPROBES依赖的警告 （部分内核不删除会error）？(y/n): " choice
-if [ "$choice" = "y" ]; then
-  sed -i '59,60d' KernelSU/kernel/ksu.c
-fi
-
-
 # AnyKernel3 路径
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
 # 编译完成后内核名字
-FINAL_KERNEL_ZIP=zen_plus-13_KernelSU_A13_dibin.zip
+FINAL_KERNEL_ZIP=perf_KernelSU_A13_dibin.zip
 # 内核工作目录
 export KERNEL_DIR=$(pwd)
 # 内核 defconfig 文件
